4878318219fd5f318fc768ba9774062d8c95f99b
ParkAsher <devasherpark@naver.com>
2022-08-15 21:59:52 +0900

    useEffect study

--- client/src/Component/List.js
+++ client/src/Component/List.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 function List(props) {
 
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <h1>List component!</h1>
             {props.ContentList.map((content, idx) => {
                 return (
                     <div key={idx} style={{ width: "100%", marginLeft: "1rem", }}>
--- client/src/Component/Upload.js
+++ client/src/Component/Upload.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Upload(props) {
 
@@ -11,10 +11,22 @@ function Upload(props) {
         setContent("");
     }
 
+    /*
+        useEffect
+
+        [] : useEffect가 실행될 조건 (빈 배열일때는 실행되거나 죽을때 딱 한번만 실행)
+    */
+
+    useEffect(() => {
+        console.log("Content가 바뀌었습니다.");
+    }, [Content]);
+
+
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <h1>Upload component!</h1>
             <input type="text" value={Content} onChange={(event) => { setContent(event.currentTarget.value) }}></input>
-            <button onClick={() => { onSubmit() }} style={{ marginTop: "1rem" }}>제출</button>
+            <button onClick={() => { onSubmit() }} >제출</button>
         </div>
     )
 }
