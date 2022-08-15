3a4a50ed1dc4a3b102a01454639cfb3503cf848b
ParkAsher <devasherpark@naver.com>
2022-08-15 12:53:47 +0900

    how to make Component

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Test from './Test';
+
+function App() {
+  return (
+    <div>
+      <h1>hello, react!</h1>
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
