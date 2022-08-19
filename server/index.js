8ffce3a80b1c076d22ac453840870b2f41d2ca0c
ParkAsher <devasherpark@naver.com>
2022-08-19 13:20:58 +0900

    post delete & sever router

--- server/index.js
+++ server/index.js
@@ -33,10 +33,11 @@ app.use(express.urlencoded({ extended: true }));
 
 
 /*
-    model
+    Router
+    /api/post : post에 관련된 Router
 */
-const { Post } = require('./Model/Post.js');
-const { Counter } = require('./Model/Counter.js');
+app.use("/api/post", require("./Router/post.js"));
+
 
 app.listen(port, () => {
     mongoose.connect('mongodb+srv://asherpark:test123@cluster0.ixgntvl.mongodb.net/Community?retryWrites=true&w=majority').then(() => {
@@ -89,68 +90,3 @@ app.post("/api/test", (req, res) => {
     npm i @emotion/css @emotion/react @emotion/styled
 
 */
-app.post("/api/post/submit", (req, res) => {
-
-    let temp = req.body;
-
-    Counter.findOne({ name: "counter" }).exec().then((counter) => {
-        temp.postNum = counter.postNum;
-
-        const CommunityPost = new Post(temp)
-
-        CommunityPost.save().then(() => {
-
-            // updateOne({어떤 doc를 업데이트할건지}, {어떻게 업데이트할건지})
-            Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } }).then(() => {
-
-                res.status(200).json({ success: true });
-
-            });
-        });
-
-    }).catch((err) => {
-
-        res.status(400).json({ success: false });
-    })
-})
-
-app.post("/api/post/list", (req, res) => {
-
-    Post.find().exec().then((doc) => {
-
-        res.status(200).json({ success: true, postList: doc });
-
-    }).catch((err) => {
-
-        res.status(400).json({ success: false })
-    })
-})
-
-app.post("/api/post/detail", (req, res) => {
-
-    Post.findOne({ postNum: Number(req.body.postNum) }).exec().then((doc) => {
-
-        res.status(200).json({ success: true, post: doc });
-
-    }).catch((err) => {
-
-        res.status(400).json({ success: false })
-    })
-})
-
-app.post("/api/post/edit", (req, res) => {
-
-    let temp = {
-        title: req.body.title,
-        content: req.body.content
-    }
-
-    Post.updateOne({ postNum: Number(req.body.postNum) }, { $set: temp }).exec().then(() => {
-
-        res.status(200).json({ success: true });
-
-    }).catch((err) => {
-
-        res.status(400).json({ success: false })
-    })
-})
