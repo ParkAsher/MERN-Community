dae04eea153a91385fecf1e1c760c62a4cb5db5a
ParkAsher <devasherpark@naver.com>
2022-08-22 17:47:23 +0900

    logout

--- client/src/Component/Heading.js
+++ client/src/Component/Heading.js
@@ -3,26 +3,50 @@ import React from 'react'
     react-router-dom [Link]
     a 태그처럼 사용 됨
 */
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 /*
     React-Bootstrap
 */
 import { Navbar, Container, Nav, } from 'react-bootstrap'
+/*
+    redux
+*/
+import { useSelector } from 'react-redux'
+/*
+    firebase
+*/
+import firebase from '../firebase.js'
 
 
 
 function Heading() {
+
+    const user = useSelector((state) => state.user)
+
+    const navigate = useNavigate();
+
+    const LogoutHandler = () => {
+        firebase.auth().signOut();
+        navigate("/");
+    }
+
+
     return (
         <Navbar bg="dark" expand="lg" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Community</Navbar.Brand>
+                <Navbar.Brand href="/">React-Community</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Link to="/" style={{ color: "white", textDecoration: "none", marginRight: "10px" }}>Home</Link>
                         <Link to="/Upload" style={{ color: "white", textDecoration: "none", marginRight: "10px" }}>Upload</Link>
-                        <Link to="/login" style={{ color: "white", textDecoration: "none", marginRight: "10px" }}>Login</Link>
                     </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className='justify-content-end'>
+                    {
+                        user.accessToken ? <Navbar.Text style={{ color: "white", cursor: "pointer" }} onClick={() => LogoutHandler()}>Logout</Navbar.Text>
+                            : <Link to="/login" style={{ color: "white", textDecoration: "none" }}>Login</Link>
+                    }
                 </Navbar.Collapse>
             </Container>
         </Navbar>
